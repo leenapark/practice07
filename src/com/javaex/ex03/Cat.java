0406384445a4cc4f9141d8ded5aea35541847fae
leenapark <hrn424@gmail.com>
2020-12-03 18:45:05 +0900

    20201203 객체지향 연습 문제 인터페이스

--- src/com/javaex/ex03/Cat.java
+++ src/com/javaex/ex03/Cat.java
@@ -1,13 +1,6 @@
 package com.javaex.ex03;
 
 public class Cat implements Soundable {
-	/*
-	public void catSound(String sound) {
-		if(sound.equals("cat")) {
-			sound();
-		}
-	}
-	*/
 
 	public void sound() {
 		System.out.println("야옹");
